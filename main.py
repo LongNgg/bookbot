8400b93595f51655f3fa76eb17fcca017299ba33
LongNgg <hoanglongnguyen610@gmail.com>
2024-07-31 17:09:03 +0700

    Edit the count_characters to return a list of dictionaries of 2 entries: char and count. Update the main() to provide proper format.

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ def main():
         file_contents = f.read()
         word_count = count_words(file_contents)
         chars = count_characters(file_contents)
+        chars.sort(reverse=True, key=sort_on)
 
         print("--- Begin report of books/frankenstein.txt ---")
-        print(f"{word_count} words found in the document")
-        for char in chars.sort():
-            print(f"The '{char}' character was found {chars[char]} times")
+        print(f"{word_count} words found in the document\n")
+
+        for char in chars:
+            if char["char"].isalpha():
+                print(f"The '{char['char']}' character was found {char['count']} times")
         print("--- End report ---")
-        # print(file_contents)
 
 
 def count_words(s):
@@ -19,13 +21,20 @@ def count_words(s):
 
 
 def count_characters(s):
+    s = s.lower()
     char_count = {}
-    for char in s.lower():
+    for char in s:
+        tmp_dict = {}
         if char not in char_count:
             char_count[char] = 1
         else:
             char_count[char] += 1
-    return char_count
+    list_chars = [{"char": key, "count": value} for key, value in char_count.items()]
+    return list_chars
+
+
+def sort_on(dict):
+    return dict["count"]
 
 
 main()
